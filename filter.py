ac2fc11195660807e74f46f5b3f461ae3a753007
JCampbell654 <125030713+JCampbell654@users.noreply.github.com>
2023-02-17 19:07:09 +0000

    Add files via upload
    Added parallax filtering in the filter script

--- filter.py
+++ filter.py
@@ -1,17 +1,8 @@
 from data_handler import *
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import simpson
 
-raw_data = Data("Data/1676314285780O-result.json")
-
-fig, (ax_raw, ax_f) = plt.subplots(1, 2)
-ax_raw.scatter(raw_data.get_pmra(), raw_data.get_pmdec(), 1)
-ax_raw.set_title("Unfiltered data")
-ax_raw.set_xlabel("Proper motion right ascension [mas.yr**-1]")
-ax_raw.set_ylabel("Proper motion declination [mas.yr**-1]")
-
-def line_intersect(x1, x2, y1, y2):
-	return x2 >= y1 and y2 >= x1
 
 def none_filter(data):
 
@@ -21,32 +12,40 @@ def none_filter(data):
 	data.data = data.data[p, :]
 	return data
 
-def parallax_filter(data, cluster_distance, cluster_radius):
+def parallax_probability_distribution(position, uncertainty):
+	x = np.linspace(position-uncertainty, position+uncertainty, 100)
+	y = (x/x)/(2*uncertainty)
+	return x, y
 
-	star_distance = 1/(data.get_parallax()*1e-3)
-	star_distance_err = 1/(data.get_parallax_err()*1e-3)
+def parallax_filter(data, distance, radius):
 
-	x1 = cluster_distance - cluster_radius
-	x2 = cluster_distance + cluster_radius
-	y1 = star_distance - (star_distance_err / 2)
-	y2 = star_distance + (star_distance_err / 2)
-	#filter_arr = np.where(line_intersect(x1, y1, x2, y2), True, False)
-	a = x2 >= y1
-	b = y2 >= x1
-	c = np.array([], dtype=bool)
-	for i in range(len(a)):
-		if (a[i] == True and b[i] == True):
-			c = np.append(c, True)
+	#Get parallax data
+	p = data.get_parallax() * 1e-3
+	p_err = data.get_parallax_err() * 1e-3
+
+	#Calculate distances from parallax data
+	star_dist = 1/p
+	star_dist_err = np.abs(p_err/(p**2))
+
+	#Find limits for possible cluster region
+	x_l = distance - radius
+	x_u = distance + radius
+	
+	probability = np.array([])
+
+	for i in range(star_dist.size):
+		x_pd, y_pd = parallax_probability_distribution(star_dist[i], star_dist_err[i])
+		_filter = np.logical_and((x_pd > x_l), (x_pd < x_u))
+		x_pd = x_pd[_filter]
+		y_pd = y_pd[_filter]
+
+		if x_pd.size == 0:
+			probability = np.append(probability, 0)
 		else:
-			c = np.append(c, False)
+			dx = (x_pd.max() - x_pd.min()) / x_pd.size
+			#Calculate area under curve
+			probability = np.append(probability, simpson(y_pd, dx=dx))
 
-	data.data = data.data[c, :]
+	return probability
 
-parallax_filter(none_filter(raw_data), 325, 4.59902)
 
-ax_f.scatter(raw_data.get_pmra(), raw_data.get_pmdec(), 1)
-ax_f.set_title("Filtered data")
-ax_f.set_xlabel("Proper motion right ascension [mas.yr**-1]")
-ax_f.set_ylabel("Proper motion declination [mas.yr**-1]")
-
-plt.show()
