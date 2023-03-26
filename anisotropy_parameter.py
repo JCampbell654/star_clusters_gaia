d267967ab40b6e51333a17abd01b75db2a0bcbb6
JCampbell654 <125030713+JCampbell654@users.noreply.github.com>
2023-03-26 16:14:17 +0100

    Update anisotropy_parameter.py

--- anisotropy_parameter.py
+++ anisotropy_parameter.py
@@ -2,8 +2,6 @@ from data_handler import *
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
-from os import listdir
-import seaborn as sns
 
 def points_in_radius(data, radius):
 
@@ -37,37 +35,22 @@ def anisotropy_parameter(data, plot=False, bins=20):
 		lowest_bin = np.ceil(data.get_data_size()/bins).astype(int)
 		min_r = np.partition(dist, lowest_bin)[lowest_bin] #Get the second smallest distance to avoid div by 0 error in the uncertainty calculation
 		r = np.linspace(min_r, max_r, bins)
+		
+		#Initialize arrays
 		ap_arr = np.array([])
 		err_arr = np.array([])
 
+		#Calculate anisotropy parameter and error for all radii
 		for i in r:
 			r_data = points_in_radius(data, i)
 			r_ap = anisotropy_parameter(r_data, plot=False)
 			ap_arr = np.append(ap_arr, r_ap)
 			err_arr = np.append(err_arr, 2 * (1-r_ap) * ((1/(r_data.get_data_size()-1))**(1/2)))
 
+		#Plot anisotropy parameter as a function of radius
 		plt.xlabel("r/R")
 		plt.ylabel(r'$\beta = 1-\sigma_t^2/\sigma_r^2$')
 		plt.errorbar(r/max_r, ap_arr, yerr=err_arr)
 
+	#Return value
 	return  1 - (np.std(pmt)**2/np.std(pmr)**2), 2 * ((np.std(pmt)**2/np.std(pmr)**2)) * ((1/(data.get_data_size()-1))**(1/2))
-
-ages = np.array([1900, 330, 67.6, 225, 175, 278.6, 190, 78, 450, 140, 3200, 6600, 30.9, 1000, 141, 160, 1800, 1700])
-masses = np.array([1698, 1937, 142, 1539, 232, 568, 453, 2366, 670, 279, 69, 234, 505])
-members = np.array([153, 188, 197, 429, 35, 225, 180, 217, 97, 456, 84, 277, 129])
-ap_arr = np.array([])
-err_arr = np.array([])
-
-for path in listdir("Filtered Data"):
-	data = Data("Filtered Data/" + path)
-	ap, err = anisotropy_parameter(data, plot=False)
-	ap_arr = np.append(ap_arr, ap)
-	err_arr = np.append(err_arr, err)
-
-
-plt.scatter(ages, ap_arr)
-plt.errorbar(ages, ap_arr, yerr = err_arr, fmt='o')
-#plt.hist(ap_arr, bins=20)
-#sns.distplot(ages, bins=10)
-plt.show()
-
