8225f6c8cd3543c214bd58340f8ce1057c1a5c56
TheCodingSeal20 <50917345+TheCodingSeal20@users.noreply.github.com>
2023-04-05 10:48:53 +0100

    Update ks_test.py
    Added code to save plots and test result, removed unused functions,

--- ks_test.py
+++ ks_test.py
@@ -10,21 +10,26 @@ Created on Wed Mar 29 15:25:21 2023
 from data_handler import *
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import simpson
 from scipy import stats
-import copy
+import pandas as pd
 
-#%% Importing Data
+#%%Files and Folders
 
-data_folder = 'C:/Users/rhbjo/OneDrive - The University of Nottingham/Shared/Filtered Data/'
-data = Data(data_folder + 'Messier 47-kstest-filtered.json')
+cluster = 'NGC 7789'
+
+data_folder = 'C:/Users/rhbjo/OneDrive - The University of Nottingham/Shared/Final Data/'
+data = Data(data_folder + f'{cluster}-final.json')
+
+plot_folder = 'C:/Users/rhbjo/OneDrive - The University of Nottingham/Shared/Cumlative Frequency Graphs/'
+ks_test_folder = 'C:/Users/rhbjo/OneDrive - The University of Nottingham/Shared/KS Test Data/'
+
+save = False
+
+#%%Cumfreq calculations
 
 pmr, pmt = data.change_basis()
 
-
-#%%cumfreq calculations
-
-def cal_cumfreq(key, velocity):
+def cal_cumfreq(cluster, key, velocity):
     numbins=100
 
     sigma = np.std(velocity)
@@ -36,28 +41,44 @@ def cal_cumfreq(key, velocity):
     
     bar_heights, x, _ = plt.hist(velocity, bins=numbins, density=True, cumulative=True, label='Velocity dist')
     
-    y = stats.norm.cdf(x, mu, sigma)
+    y = stats.norm.cdf(x, mu, sigma) # creating normal distribution
     
-    #plotting
+    #plotting normal distribution
     plt.plot(x, y, lw=2, c='r', label='Normal dist')
     ax.set_xlabel(f'{key} velocity')
     ax.set_ylabel('cumfreq density')
     ax.legend()
+    
     plt.show()
+    if save == True:
+        plt.savefig(plot_folder + f'{cluster}-{key}.png')
         
     return mu, sigma
 
 #%%ks-test
+test_statistics = []
+p_values = []
 
-def ks_test(key, velocity):
-    mu, sigma = cal_cumfreq(key, velocity)
+def ks_test(cluster, key, velocity):
+    mu, sigma = cal_cumfreq(cluster, key, velocity)
     ks_test = stats.ks_1samp(velocity, stats.norm.cdf, args=(mu, sigma))
+   
     test_statistic = ks_test.statistic
-    p_val = ks_test.pvalue
-    print(test_statistic, p_val)
+    p_value = ks_test.pvalue
+    
+    print(test_statistic, p_value)
+    test_statistics.append(test_statistic)
+    p_values.append(p_value)
 
-ks_test('pmr', pmr)
-ks_test('pmt', pmt)
+ks_test(cluster, 'pmr', pmr)
+ks_test(cluster, 'pmt', pmt)
+
+#%%Saving KS test results
+
+if save == True:
+    d = {'Test Statistic': test_statistics, 'P Value': p_values}
+    df = pd.DataFrame(data=d, index=['pmr', 'pmt'])
+    df.to_json(ks_test_folder + f'{cluster}-KS-test-result.json')
 
 
 
